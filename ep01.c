5d578305dcf13f7d8740d01cdabf712f0117e828
ppc19 <ppc19@inf.ufpr.br>
2021-10-07 20:37:20 -0300

    pequenas mudanças

--- ep01.c
+++ ep01.c
@@ -9,15 +9,26 @@
 
 #define NEWTON_NEXTVAL "(fx + fx1)/fx1"
 
-/***********************
+/*****************************************************************
  ESTRUTURA DE ITERAÇÕES
-************************/
+ Matriz que armazena dados das iterações do cálculo de raízes
+ por um método numérico.
+ 
+ size: número de linhas da matriz
+ max_iter: Número máximo de iterações permitido para a matriz
+ iteration: matriz de iterações.
+    - iteration[i][0]: x aproximado na iteração i
+    - iteration[i][1]: valor do critério de parada na iteração i
+*****************************************************************/
 typedef struct {
     int size;
     int max_iter;
-    int iteration[ITER_LIMIT][3];
+    double iteration[ITER_LIMIT][2];
 } iterations_t;
 
+/* 
+    Cria e inicializa instância da estrutura 
+*/
 iterations_t *new_iterations_t(int max_iter)
 {
     iterations_t *new_it = malloc(sizeof(iterations_t));
@@ -27,27 +38,35 @@ iterations_t *new_iterations_t(int max_iter)
     return new_it;
 }
 
-void *destroy_iterations_t(iterations_t *max_iter)
+/* 
+    Desaloca estrutura de iterações 
+*/
+void *destroy_iterations_t(iterations_t *it)
 {
-    if (max_iter)
-        free(max_iter);
+    if (it)
+        free(it);
 }
 
+/*
+    Inclui uma iteração no final da matriz  
+*/
 void push_iteration(iterations_t *it, double aprox, double crit)
 {
     int last = it->size;
     if (it->size < it->max_iter){
-        it->iteration[last][0] = last;
-        it->iteration[last][1] = aprox;
-        it->iteration[last][2] = crit;
+        it->iteration[last][0] = aprox;
+        it->iteration[last][1] = crit;
         (it->size)++;
     }
 }
 
-/**************************
- FUNÇÕES DE ZEROS
-**************************/
+/*******************************
+ FUNÇÕES PARA CÁLCULO DE ZEROS
+*******************************/
 
+/* 
+    Calcula valor de f(x) usando matheval
+*/
 double calculate_function(void *f, double x)
 {
     char **names;
@@ -57,17 +76,34 @@ double calculate_function(void *f, double x)
     double f_x = evaluator_evaluate(f, count, names, values);
 }
 
-double next_value(double x, void *f, void *f_prim)
+/* 
+    Calcula próximo valor de x pelo método Newton-Raphson 
+*/
+double next_value_newton(double x, void *f, void *f_prim)
 {
-    
+    return 1.0;
 }
 
+/* 
+    Calcula próximo valor de x pelo metodo da secante 
+*/
+double next_value_secante(double x, void *f, void *f_prim)
+{
+    return 1.0;
+}
+
+/* 
+    Calcula critério de parada para valor atual e próximo 
+*/
 double calculate_error(double cur, double next)
 {   
-    // abs((next -cur)/next) * 100 
+    // abs((next - cur)/next) 
     return abs(1 - cur/next);
 }
 
+/* 
+    Calcula zero da função e retorna matriz com iterações
+*/
 iterations_t *calculate_zero(void *f, double x0, double epsilon, int max_iter)
 {
     iterations_t *iterations = new_iterations_t(max_iter);
@@ -77,7 +113,7 @@ iterations_t *calculate_zero(void *f, double x0, double epsilon, int max_iter)
     assert(f_prim);
 
     // Calcular iterações
-    double x1 = next_value(x0, f, f_prim);
+    double x1 = next_value_newton(x0, f, f_prim);
     double error = calculate_error(x0, x1);
 
     push_iteration(iterations, x0, error);
@@ -100,7 +136,9 @@ int main()
     assert(f);
 
     iterations_t *newton_it = calculate_zero(f, x0, epsilon, max_iter);
-    // iterations_t *sec_it = calculate_zero(f, x0, epsilon, max_iter);
+    //iterations_t *sec_it = calculate_zero(f, x0, epsilon, max_iter);
+
+    printf("%1.16e %1.16e\n", newton_it->iteration[0][0], newton_it->iteration[0][1]);
 
     evaluator_destroy(f);
     return 0;
